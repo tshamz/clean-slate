daa97f9cde3d77889274ee8f963860dd12cb8f8d
Tyler Shambora <tyler@theshamboras.com>
2018-11-07 09:55:21 -0800

    update error in screenshots.js

--- test/screenshot/screenshot.js
+++ test/screenshot/screenshot.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   const now = new Date();
   const dateOptions = { day: '2-digit', month: '2-digit', year: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'};
-  const fileName = now.toLocaleDateString("en-US", dateoptions)
+  const fileName = now.toLocaleDateString("en-US", dateOptions)
 
   await page.goto('https://buddy.works');
   await page.screenshot({path: `${fileName}.png`});
