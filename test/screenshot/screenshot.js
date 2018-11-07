1cd4779a089fd999ae8e312a2478e54833072f71
Tyler Shambora <tyler@theshamboras.com>
2018-11-07 09:42:47 -0800

    add test script

---
+++ test/screenshot/screenshot.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const page = await browser.newPage();
+  const now = new Date();
+  const dateOptions = { day: '2-digit', month: '2-digit', year: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'};
+  const fileName = now.toLocaleDateString("en-US", dateoptions)
+
+  await page.goto('https://buddy.works');
+  await page.screenshot({path: `${fileName}.png`});
+
+  await browser.close();
+})();
