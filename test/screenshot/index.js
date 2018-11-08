d9712b9fb9c557d66d7c724c635db5141a0cb8e8
Tyler Shambora <tyler@theshamboras.com>
2018-11-07 18:24:52 -0800

    update test

--- test/screenshot/index.js
+++ test/screenshot/index.js
@@ -1,26 +1,43 @@
 const puppeteer = require('puppeteer');
 
 const takeScreenshot = async () => {
-  try {
-    const url = process.env.BUDDY_PIPELINE_TARGET_SITE_URL;
-    console.log(url);
+  // try {
+  //   const url = process.env.BUDDY_PIPELINE_TARGET_SITE_URL;
+  //   console.log(url);
 
-    if (!url) {
-      return false;
-    }
+  //   if (!url) {
+  //     return false;
+  //   }
 
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
+  //   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  //   const page = await browser.newPage();
 
-    const path = `/test/screenshot/images/before/${process.env.BUDDY_EXECUTION_ID}.png`;
+  //   const path = `/test/screenshot/images/before/${process.env.BUDDY_EXECUTION_ID}.png`;
 
-    await page.goto(url);
-    await page.screenshot({ path });
+  //   await page.goto(url);
+  //   await page.screenshot({ path });
 
-    await browser.close();
-  } catch (error) {
-    console.log(error);
+  //   await browser.close();
+  // } catch (error) {
+  //   console.log(error);
+  // }
+
+  const url = process.env.BUDDY_PIPELINE_TARGET_SITE_URL;
+  console.log(url);
+
+  if (!url) {
+    return false;
   }
+
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const page = await browser.newPage();
+
+  const path = `/test/screenshot/images/before/${process.env.BUDDY_EXECUTION_ID}.png`;
+
+  await page.goto(url);
+  await page.screenshot({ path });
+
+  await browser.close();
 };
 
 module.exports = takeScreenshot;
